97fd309fdc2bad4d831332392491b0bbfb6c4dc8
GURU <mjackpot47@gmail.com>
2025-10-12 14:28:18 +0500

    Update main.js

--- BGAV/static/Aviator/main.js
+++ BGAV/static/Aviator/main.js
@@ -4,7 +4,7 @@ const printSignal = document.getElementById("print-signal");
 const stopProgress = document.getElementById("stop-progress");
 
 // Статические значения для минимального и максимального коэффициентов
-const MIN_COEFFICIENT = 3.89; 
+const MIN_COEFFICIENT = 1.89; 
 const MAX_COEFFICIENT = 3.89;
 
 // Функция для генерации случайного числа
@@ -52,6 +52,7 @@ function goTimer(time) {
     }
   }, 1000);
 }
+
 
 
 
