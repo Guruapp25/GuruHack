122058a1f0f58c5a5a14a9ad240bc648faa65e5c
GURU <mjackpot47@gmail.com>
2025-10-13 15:49:44 +0500

    Update main.js

--- Aviator/static/Aviator/main.js
+++ Aviator/static/Aviator/main.js
@@ -4,8 +4,8 @@ const printSignal = document.getElementById("print-signal");
 const stopProgress = document.getElementById("stop-progress");
 
 // Статические значения для минимального и максимального коэффициентов
-const MIN_COEFFICIENT = 3.69; 
-const MAX_COEFFICIENT = 3.69;
+const MIN_COEFFICIENT = 3.50; 
+const MAX_COEFFICIENT = 3.50;
 
 // Функция для генерации случайного числа
 function getRandomFloat(min, max, decimals) {
@@ -52,6 +52,7 @@ function goTimer(time) {
     }
   }, 1000);
 }
+
 
 
 
